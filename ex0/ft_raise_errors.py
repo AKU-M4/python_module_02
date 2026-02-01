1811d489b575cb66028276dba2c552b35b49c402
Adam Kaid salim <adkaid-s@c3r5p4.1337.ma>
2026-02-01 18:53:47 +0100

    baabaaboiii

---
+++ ex0/ft_raise_errors.py
@@ -0,0 +1,25 @@
+def check_temperature(temp_str):
+    """ Basic function that tests temperature and checks if it's valid """
+    print(f"Testing Temperature: {temp_str}")
+    try:
+        temp = int(temp_str)
+        if temp < 0:
+            print(f"Error: {temp} is too cold for a plants (min 0°C)")
+        elif temp > 40:
+            print(f"Error: {temp} is too hot for a plants (min 40°C)")
+        else:
+            print(f"Temperature {temp} is perfect for plants!")
+
+    except:
+        print(f"Error: {temp_str} is not a valid number")
+
+
+if __name__ == "__main__":
+    print("=== Garden temperature Checker ===")
+
+    values = {"abc", "40", "60", "-50"}
+    for s in values:
+        check_temperature(s)
+        print()
+
+    print("All tests completed - program didn't crash!")
---
+++ ex1/ft_different_errors.py
@@ -0,0 +1,50 @@
+def test_error_types():
+    """ Testing function to handle all the required errors. """
+    print("=== Garden Error Types Demo ===")
+
+    print("Testing ValueError...")
+    try:
+        int("abc")  # Hardcode the bad value here
+    except ValueError as e:
+        print(f"Caught ValueError: {e}\n")
+
+    # 2. ZeroDivisionError
+    print("Testing ZeroDivisionError...")
+    try:
+        # Use numbers here, not the 'test' string variable
+        result = 10 / 0
+    except ZeroDivisionError as e:
+        print(f"Caught ZeroDivisionError: {e}\n")
+
+    # 3. FileNotFoundError
+    print("Testing FileNotFoundError...")
+    try:
+        f = open("missing_file.txt", "r")
+    except FileNotFoundError as e:
+        print(f"Caught FileNotFoundError: {e}\n")
+
+    # 4. KeyError
+    print("Testing KeyError...")
+    try:
+        # Dictionary needs {Key: Value} syntax, not just {"Key"}
+        my_plants = {"Sunflower": "Yellow", "Rose": "Red"} 
+        print(my_plants["Orchid"]) # Try to access a key that isn't there
+    except KeyError as e:
+        print(f"Caught KeyError: {e}\n")
+
+    # 5. Multiple Errors Catching
+    print("Testing multiple errors together...")
+    try:
+        result = 10 / 0
+    except (ValueError, ZeroDivisionError, FileNotFoundError):
+        print("Caught an error, but program continues!\n")
+
+    print("All error types tested successfully!")
+
+def garden_operations():
+    """ Function to excectute all the tests errors and print them. """
+    # You only need to call this once, and it runs all tests sequentially
+    test_error_types()
+
+if __name__ == "__main__":
+    garden_operations()
